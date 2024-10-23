f8db412aba499cf318322d1f3c066bc82804727d
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-10-23 15:50:06 +0200

    refactor: Remove Footer component from Guide pages

--- src/views/Guide/ParticularGuide.vue
+++ src/views/Guide/ParticularGuide.vue
@@ -3,18 +3,15 @@
     <GuideContent :categories="['service-prostitution', 'service-prostitution-mineur']" />
   </div>
 
-<Footer />
 </template>
 
 <script>
 import GuideContent from "../../components/Guide/GuideContent.vue"
 import NavigationBar from "../../components/NavigationBar.vue";
-import Footer from "../../components/Footer.vue"
 export default {
     components: {
         GuideContent,
         NavigationBar,
-        Footer,
     }
     
     };
--- src/views/Guide/ProfessionalGuide.vue
+++ src/views/Guide/ProfessionalGuide.vue
@@ -1,17 +1,14 @@
 <template>
     <GuideContent :categories="['professionnel-prostitution', 'professionnel-prostitution-mineur']" />
-    <Footer />
     </template>
     
     <script>
     import GuideContent from "../../components/Guide/GuideContent.vue"
     import NavigationBar from "../../components/NavigationBar.vue";
-    import Footer from "../../components/Footer.vue"
     export default {
         components: {
             GuideContent,
             NavigationBar,
-            Footer,
         }
         
         };
