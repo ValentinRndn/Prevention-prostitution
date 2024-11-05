0ab3cfb0b2504b9fb40e9152d774fb477c215d13
Valentin renaudin <134637295+ValentinRndn@users.noreply.github.com>
2024-11-05 12:33:13 +0100

    refactor: Update footer design and content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,5 @@
 <template>
-  <footer class="bg-grey text-white font-semibold border-t border-grey font-cgothic mx-auto flex flex-col items-center text-center">
-    <div class="container mx-auto ">
+  <footer class="bg-grey  text-white border-t relative font-cgothic mx-auto flex flex-col items-center ">
 
 
       <!-- Bandeau blanc avec les logos -->
@@ -9,14 +8,31 @@
         <img src="../assets/logo_calvados.png" alt="Logo Préfecture de Région" class="h-28  object-cover" />
         <img src="../assets/welcome/logo-region-normandie.png" alt="Logo Consortium" class="h-28  object-cover" />
       </div>
-            <!-- Texte du footer -->
-            <p class="text-sm mb-4">
-        © 2024 AGRASC - <router-link to="/legalNotices" class="underline">Mentions légales</router-link> - Pilotage : Accueil Service & PAPSP de Caen – Création : à compléter – Maintenance : Valentin RENAUDIN – Tous droits réservés
-      </p>
-      
-      <!-- Lien pour les préférences cookies -->
-      <a href="#" @click.prevent="openCookieSettings" class="text-sm underline mb-4">Préférences Cookies</a>
-    </div>
+
+
+
+      <div class="footer-bottom-container flex justify-evenly w-full my-12">
+      <div class="left-footer space-y-2 mb-4">
+  <p class="">
+    Pilotage : Accueil Service & PAPSP de Caen
+  </p>
+
+  <p class="">
+    Création : <a href="https://valentin-renaudin.com" class="hover:underline">Valentin RENAUDIN</a>
+  </p>
+
+  <p class="">
+    Maintenance : <a href="https://valentin-renaudin.com" class="hover:underline">Valentin RENAUDIN</a>
+  </p>
+</div>
+
+<div class="right-footer h-full space-y-2 flex flex-col">
+  <router-link to="/legalNotices" class="hover:underline">Mentions légales</router-link>
+    <a href="#" @click.prevent="openCookieSettings" class="hover:underline ">Préférences Cookies</a>
+</div>
+
+</div>
+<p class="ont-semibold fixed bottom-0 left-1/2 transform -translate-x-1/2">© 2024 AGRASC – Tous droits réservés</p>
   </footer>
 </template>
 
@@ -33,11 +49,6 @@ export default {
 
 <style scoped>
 /* Styles personnalisés */
-.bg-darkpurple {
-  background-color: #2a1b3d; /* Couleur sombre pour contraster avec le texte blanc */
-}
-footer p {
-  max-width: 600px;
-  line-height: 1.6;
-}
+
+
 </style>
