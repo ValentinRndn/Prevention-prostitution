e116dba209291b3452e18ad0b9dc4d5a503b2bf2
Valentin renaudin <134637295+ValentinRndn@users.noreply.github.com>
2025-05-20 14:12:25 +0200

    chore: Update axios and vite dependencies, and improve code formatting in GuideContent.vue

--- src/components/Guide/GuideContent.vue
+++ src/components/Guide/GuideContent.vue
@@ -6,14 +6,19 @@
         v-for="(category, index) in categories"
         :key="index"
         @click="loadCategory(category)"
-        :class="['button-prostitution border border-solid border-light-grey py-2 px-6 text-lg font-cgothic', { 'bg-purple text-white': selectedCategory === category }]"
+        :class="[
+          'button-prostitution border border-solid border-light-grey py-2 px-6 text-lg font-cgothic',
+          { 'bg-purple text-white': selectedCategory === category },
+        ]"
       >
         {{ formatCategory(category) }}
       </button>
     </div>
 
     <!-- Conteneur des documents -->
-    <div class="documents-container mt-14 mx-10 flex flex-wrap gap-5 justify-center sm:justify-center">
+    <div
+      class="documents-container mt-14 mx-10 flex flex-wrap gap-5 justify-center sm:justify-center"
+    >
       <div
         v-for="doc in documents"
         :key="doc.id"
@@ -29,11 +34,15 @@
         </a>
         <!-- Détails -->
         <a :href="doc.link" target="_blank" rel="noopener noreferrer">
-          <h3 class="text-lg font-semibold max-w-[250px] mx-auto overflow-hidden text-ellipsis whitespace-nowrap">
+          <h3
+            class="text-lg font-semibold max-w-[250px] mx-auto overflow-hidden text-ellipsis whitespace-nowrap"
+          >
             {{ doc.title }}
           </h3>
         </a>
-        <p class="text-sm max-w-[250px] mx-auto overflow-hidden text-ellipsis whitespace-nowrap">
+        <p
+          class="text-sm max-w-[250px] mx-auto overflow-hidden text-ellipsis whitespace-nowrap"
+        >
           {{ doc.description }}
         </p>
       </div>
@@ -41,55 +50,65 @@
   </div>
 </template>
 
-
-
-
-
 <script>
-import { getGuideByCategory } from '../../services/GuideService.js';
+import { getGuideByCategory } from "../../services/GuideService.js";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 export default {
-  name: 'GuideContent',
+  name: "GuideContent",
   props: {
     categories: {
       type: Array,
-      required: true
-    }
+      required: true,
+    },
   },
   data() {
     return {
       documents: [],
-      selectedCategory: ''
+      selectedCategory: "",
     };
   },
   methods: {
     formatCategory(category) {
-    return category.replace(/-/g, ' ').replace(/\b\w/g, char => char.toUpperCase());
-  },
+      return category
+        .replace(/-/g, " ")
+        .replace(/\b\w/g, (char) => char.toUpperCase());
+    },
     async loadCategory(category) {
       this.selectedCategory = category;
       try {
         const response = await getGuideByCategory(category);
-        this.documents = response.map(doc => ({
-          id: doc._id,
-          title: doc.title,
-          description: doc.description,
-          category: doc.category,
-          image: `${apiUrl}/${doc.image}`,
-          pdf: `${apiUrl}/${doc.pdf}`  
-        }));
+
+        // Trier les documents par date en se basant sur le timestamp contenu dans le nom du PDF
+        this.documents = response
+          .sort((a, b) => {
+            const getTimestamp = (pdfPath) => {
+              const match = pdfPath.match(/(\d{13})/); // 13 chiffres = timestamp en ms
+              return match ? parseInt(match[1]) : 0;
+            };
+            return getTimestamp(b.pdf) - getTimestamp(a.pdf);
+          })
+          .map((doc) => ({
+            id: doc._id,
+            title: doc.title,
+            description: doc.description,
+            category: doc.category,
+            image: `${apiUrl}/${doc.image}`,
+            pdf: `${apiUrl}/${doc.pdf}`,
+          }));
       } catch (error) {
-        console.error('Error fetching documents:', error);
+        console.error("Error fetching documents:", error);
       }
-    }
+    },
   },
   mounted() {
     if (this.categories.length > 0) {
       this.loadCategory(this.categories[0]);
     }
-    console.log('Mounted documents:', this.documents.map(doc => ({ pdf: doc.pdf, image: doc.image })));
-  }
+    console.log(
+      "Mounted documents:",
+      this.documents.map((doc) => ({ pdf: doc.pdf, image: doc.image }))
+    );
+  },
 };
 </script>
-
